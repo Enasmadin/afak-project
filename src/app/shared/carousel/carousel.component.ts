aec39562aedba8b7446008c6366376f1f2424f16
enasmadina <enasmadina4@gmail.com>
2023-05-28 16:02:50 +0200

    last-home

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -7,26 +7,77 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
+  appSettings = {
+    "settings": {
+      "rtl": 1,
+      "ltr": 0,
+
+    }
+  }
+  partner_list = [
+    
+    
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_20.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_21.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_22.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_23.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_24.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_25.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_26.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_27.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_28.png'
+    },
+    {
+      logo_att_name: '../../../assets/imag/home/carsol/partener_29.png'
+    },
+    
+
+
+  ]
+
+
+
+
   customOptions: OwlOptions = {
     loop: true,
+    autoplay: true,
+   
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
+    margin: 5,
+    
+
     dots: false,
     navSpeed: 700,
-    navText: ['', ''],
+    navText: [
+      
+      "<i class='material-icons'>chevron_left</i>",
+      "<i class='material-icons'>navigate_next</i>",
+
+    ],
+
     responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
+
+      600: {
+        items: 7
       }
     },
     nav: true
